fc9bf19d1baab1713cca673692c1f5dd593dc6d1
william fenji <shinichi.fenji@gmail.com>
2016-12-04 13:13:50 +0700

    macau45toto changes
    - avoid mb_detect_encoding function.
    - change font color.
    - create auto toto result in macau45toto.

--- assets/js/macau45toto-live.js
+++ assets/js/macau45toto-live.js
@@ -1,16 +1,52 @@
 function populate(){
+	var toto = [];
+	var rstpos = 0;
+	var addition = 0;
 	$.ajax("macau45toto_4d.php").done(function(data) {
 		var part = data.split(',');
 		$.each(part, function(key, value) {
-			$("#data-"+key).text(value);
+			if(value.trim() == ''){
+				value = '&nbsp;';
+			}
+			$("#data-"+key).html(value);
 		});
 	})
 	$.ajax("macau45toto_toto.php").done(function(data) {
 		var part = data.split(',');
 		$.each(part, function(key, value) {
-			$("#toto-"+key).text(value);
+			if(value.trim() == ''){
+				value = '&nbsp;';
+			}
+			else if(key > 1 && key < 8){
+				toto.push(value);
+			}
+			else if(key == 8){
+				toto.sort();
+				toto.push(value);
+				console.log(toto);
+				rstpos = key + 1;
+			}
+			$("#toto-"+key).html(value);
 		});
+		if(rstpos != 0){
+			get_result(toto, rstpos);
+		}
 	})
+}
+function get_result(toto, rstpos){
+	var t = parseInt(toto[1])+parseInt(toto[2]);
+	var o = parseInt(toto[3])+parseInt(toto[4]);
+	var toto1 = t + o;
+	var to = parseInt(toto[0])+parseInt(toto[5])+parseInt(toto[6]);
+	
+	to = to + toto1 * 2;
+	to = to.toString();
+	to = to.substr(parseInt(to.length)-2);
+	t = t.toString();
+	t = t.substr(parseInt(t.length)-1);
+	o = o.toString();
+	o = o.substr(parseInt(o.length)-1);
+	$("#toto-"+rstpos).html(t+o+to);
 }
 $(function() {
 	populate();
